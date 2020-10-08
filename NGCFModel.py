f11c733381691a07b77afe959327c3ee8d50749b
zcc <zhuchen@my.swjtu.edu.cn>
2020-10-08 15:15:55 +0800

    first commit

---
+++ NGCFModel.py
@@ -0,0 +1,114 @@
+'''
+pytorch version of NGCF
+@author zhuchen  zhuchen@my.swjtu.edu.cn 2020/3/7
+'''
+import torch
+import torch.nn as nn
+from torch.nn import Module
+import torch.nn.functional as F
+from scipy.sparse import coo_matrix
+from scipy.sparse import vstack
+from scipy import sparse
+import numpy as np
+import os
+import sys
+from data_process import *
+
+class NGCF(nn.Module):
+    def __init__(self, n_users, n_items, embedding_dim, weight_size, dropout_list, norm_adj, batch_size, decay):
+        super().__init__()
+        self.name = 'NGCF'
+        self.n_users = n_users
+        self.n_items = n_items
+        self.embedding_dim = embedding_dim
+        self.weight_size = weight_size
+        self.n_layers = len(self.weight_size)
+        self.dropout_list = nn.ModuleList()
+        self.GC_Linear_list = nn.ModuleList()
+        self.Bi_Linear_list = nn.ModuleList()
+        self.batch_size = batch_size
+        self.norm_adj = norm_adj
+        self.norm_adj = self.sparse_mx_to_torch_sparse_tensor(self.norm_adj).float()
+        self.norm_adj = self.norm_adj.cuda()
+        self.decay = decay
+        self.u_f_embeddings = None   #最终得到的嵌入特征
+        self.i_f_embeddings = None
+        self.weight_size = [self.embedding_dim] + self.weight_size
+        for i in range(self.n_layers):
+            self.GC_Linear_list.append(nn.Linear(self.weight_size[i], self.weight_size[i+1]))
+            self.Bi_Linear_list.append(nn.Linear(self.weight_size[i], self.weight_size[i+1]))
+            self.dropout_list.append(nn.Dropout(dropout_list[i]))
+        
+        for i in range(self.n_layers):
+            nn.init.xavier_uniform_(self.GC_Linear_list[i].weight)
+            nn.init.zeros_(self.GC_Linear_list[i].bias)
+            nn.init.xavier_uniform_(self.Bi_Linear_list[i].weight)
+            nn.init.zeros_(self.Bi_Linear_list[i].bias)
+
+        self.user_embedding = nn.Embedding(n_users, embedding_dim)
+        self.item_embedding = nn.Embedding(n_items, embedding_dim)
+
+        self._init_weight_()
+
+    def _init_weight_(self):
+        nn.init.xavier_uniform_(self.user_embedding.weight)
+        nn.init.xavier_uniform_(self.item_embedding.weight)
+
+    def forward(self, adj):
+        ego_embeddings = torch.cat((self.user_embedding.weight, self.item_embedding.weight), dim=0)
+        all_embeddings = [ego_embeddings]
+        for i in range(self.n_layers):
+            side_embeddings = torch.sparse.mm(adj, ego_embeddings)
+            sum_embeddings = F.leaky_relu(self.GC_Linear_list[i](side_embeddings))
+            bi_embeddings = torch.mul(ego_embeddings, side_embeddings)
+            bi_embeddings = F.leaky_relu(self.Bi_Linear_list[i](bi_embeddings))
+            ego_embeddings = sum_embeddings + bi_embeddings
+            ego_embeddings = self.dropout_list[i](ego_embeddings)
+
+            norm_embeddings = F.normalize(ego_embeddings, p=2, dim=1)
+            all_embeddings += [norm_embeddings]
+
+        all_embeddings = torch.cat(all_embeddings, dim=1)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.n_users, self.n_items], dim=0)
+        return u_g_embeddings, i_g_embeddings
+    
+    def predict(self, userIDx, itemIDx):
+        with torch.no_grad():
+            if self.u_f_embeddings is not None:
+                uEmbd, iEmbd = self.u_f_embeddings, self.i_f_embeddings
+            else:
+                uEmbd, iEmbd = self.forward(self.norm_adj)
+                self.u_f_embeddings, self.i_f_embeddings = uEmbd, iEmbd
+            uembd = uEmbd[userIDx]
+            iembd = iEmbd[itemIDx]
+            prediction = torch.sum(torch.mul(uembd,iembd),dim=1)
+        return prediction
+
+
+    def   sparse_mx_to_torch_sparse_tensor(self, sparse_mx):
+        """Convert a scipy sparse matrix to a torch sparse tensor."""
+        sparse_mx = sparse_mx.tocoo().astype(np.float32)
+        indices = torch.from_numpy(
+            np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+        values = torch.from_numpy(sparse_mx.data)
+        shape = torch.Size(sparse_mx.shape)
+        return torch.sparse.FloatTensor(indices, values, shape)
+
+    def BPR_loss(self, users, pos_item, neg_item):
+        uEmbd, iEmbd = self.forward(self.norm_adj)
+        self.u_f_embeddings, self.i_f_embeddings = uEmbd, iEmbd
+        uembd = uEmbd[users]
+        pos_iembd = iEmbd[pos_item]
+        neg_iembd = iEmbd[neg_item]
+
+        pos_score = torch.sum(torch.mul(uembd,pos_iembd),dim=1) 
+        neg_score = torch.sum(torch.mul(uembd,neg_iembd),dim=1) 
+        regularizer = torch.sum(uembd**2)/2.0 + torch.sum(pos_iembd**2)/2.0 + torch.sum(neg_iembd**2)/2.0 
+        # 参照NGCF loss的设置
+        maxi = torch.log(torch.sigmoid(pos_score - neg_score))
+        
+        mf_loss = torch.mean(maxi) * -1.0
+        #print('mf_loss', mf_loss.item())
+        reg_loss = self.decay * regularizer / self.batch_size
+        #print('reg_loss', reg_loss.item())
+        return mf_loss, reg_loss
---
+++ NGCF_recommendation.py
@@ -0,0 +1,147 @@
+from NGCFModel import *
+from data_process import *
+import torch
+from torch import nn as nn
+from scipy.sparse import coo_matrix
+import numpy as np
+from numpy import diag
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.optim import Adam
+from torch.nn import MSELoss
+from metrics import *
+import os
+import datetime
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+batch_size = 4096
+BATCH_SIZE = batch_size
+epochs = 400
+para = {
+    'lr':0.0001,
+    'train':0.8
+}
+
+
+def test(model, users_list):
+    all_precision_20, all_recall_20 = [], []
+    count = 0
+    for j in range(BATCH_SIZE):
+        id = users_list[np.random.randint(data.n_users, size=1)[0]]
+        item_list = list(set(range(data.n_items)) - set(data.train_items[id]))
+        #print(len(item_list))
+        users = [id for j in range(len(item_list))]
+        users = torch.tensor(users).cuda()
+        items = torch.tensor(item_list).cuda()
+        pred = model.predict(users, items)
+        #pred = torch.squeeze(pred)
+        _, item_key = pred.sort(descending = True)
+        item_key = item_key.cpu().int()
+        item_top20 = item_key[:20]
+        item_list = np.array(item_list)
+        pred_top20 = item_list[item_top20]
+        actual = data.test_set[id]
+        precision_20 = precisionk(actual, pred_top20)
+        recall_20 = recallk(actual, pred_top20)
+        all_precision_20.append(precision_20)
+        all_recall_20.append(recall_20)
+
+    return np.mean(all_precision_20), np.mean(all_recall_20)
+
+def main():
+    model = NGCF(user_nums, item_nums, 64, [64, 64, 64], [0.1, 0.1, 0.1], norm_adj, 4096, 0.00001).cuda()
+    optim = Adam(model.parameters(), lr=para['lr'])
+    #optim = Adam(model.parameters(), lr=para['lr'],weight_decay=0.001)
+    lossfn = model.BPR_loss
+    print('start training')
+    
+    
+    
+    for i in range(epochs): #n_epochs // batch_size
+        loss_value = 0
+        mf_loss_value, reg_loss_value = 0.0, 0.0
+        t0 = time()
+        for j in range(n_epochs//batch_size +1):
+            users, pos_items, neg_items = data.sample()
+            users = torch.tensor(users).cuda()
+            pos_items = torch.tensor(pos_items).cuda()
+            neg_items = torch.from_numpy(np.array(neg_items)).cuda()
+            optim.zero_grad()
+            mf_loss, reg_loss = lossfn(users, pos_items, neg_items)
+            loss = mf_loss + reg_loss
+            loss.backward()
+            optim.step()
+            loss_value += loss.item()
+            mf_loss_value += mf_loss.item()
+            reg_loss_value += reg_loss.item()
+        
+        
+        if (i+1)%10 !=0:
+            str1 = 'epoch: %d %.5f=%.2f+%.5f' % (i, loss_value, mf_loss_value, reg_loss_value)
+            print(str1)
+            continue
+        
+        t1 = time()
+        print('one epoch consume %.2f s' % (t1-t0))
+        user_to_test = list(data.test_set.keys())
+        precision_20, recall_20 = test(model, user_to_test)
+        t2 = time()
+        print('test time %.2f s' % (t2-t1))
+        str1 = 'epoch: %d %.2f=%.2f+%.2f %.5f %.5f' % (i, loss_value, mf_loss_value, reg_loss_value, precision_20, recall_20)
+        print(str1)
+        
+    
+    all_precision_10, all_recall_10 = [], []
+    all_precision_20, all_recall_20 = [], []
+    all_ndcg_20 = []
+    pred20 = np.zeros((user_nums, 20))
+    for i in range(user_nums):
+        item_list = list(set(range(data.n_users)) - set(data.train_items[i]))
+        #print(len(item_list))
+        users = [i for j in range(len(item_list))]
+        users = torch.tensor(users).cuda()
+        items = torch.tensor(item_list).cuda()
+        pred = model.predict(users, items)
+        #pred = torch.squeeze(pred)
+        _, item_key = pred.sort(descending = True)
+        item_key = item_key.cpu().int()
+        item_top10 = item_key[:10]
+        item_top20 = item_key[:20]
+        item_list = np.array(item_list)
+        pred_top10 = item_list[item_top10]
+        pred_top20 = item_list[item_top20]
+        pred20[i] = np.array(pred_top20)
+        actual = data.test_set[i]
+        precision_10 = precisionk(actual, pred_top10)
+        recall_10 = recallk(actual, pred_top10)
+        all_precision_10.append(precision_10)
+        all_recall_10.append(recall_10)
+        precision_20 = precisionk(actual, pred_top20)
+        recall_20 = recallk(actual, pred_top20)
+        ndcg_20 = ndcgk(actual, pred_top20, 20)
+        all_precision_20.append(precision_20)
+        all_recall_20.append(recall_20)
+        all_ndcg_20.append(ndcg_20)
+        print('recall_20: %.5f ndcg_20: %.5f' % (recall_20, ndcg_20))
+        print('uid %d: pre@10 %.5f recall@10 %.5f, pre@20 %.5f recall@20 %.5f ndcg@20 %.5f' % (i, np.mean(all_precision_10), np.mean(all_recall_10),\
+            np.mean(all_precision_20), np.mean(all_recall_20), np.mean(all_ndcg_20)))
+ 
+
+
+def ensureDir(dir_path):
+    d = os.path.dirname(dir_path)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+if __name__ == '__main__':
+    cur_dir = os.getcwd()  
+    path = str(cur_dir) + '/data/gowalla'
+    data = Data(path, batch_size)
+    #data.negative_pool()
+    user_nums, item_nums = data.get_num_users_items()
+    USER_NUM, ITEM_NUM = user_nums, item_nums
+    n_epochs = data.get_trainNum()
+    plain_adj, norm_adj, mean_adj = data.get_adj_mat()   # 返回的是拉普拉斯矩阵的三个形式， 具体看source code
+    
+    main()
